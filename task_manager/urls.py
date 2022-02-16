910ea4b01fe9c232e8eb221f14f9aba8e412e2d2
Ravinder Singh <ravindermaan0123@gmail.com>
2022-02-16 20:35:25 +0530

    milestone completed

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -16,7 +16,34 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.contrib.auth.views import LogoutView
+
+from tasks.views import (
+    GenericTaskCreateView,
+    GenericTaskDeleteView,
+    GenericTaskDetailView,
+    GenericTaskUpdateView,
+    GenericTaskView,
+    UserCreateView,
+    UserLoginView,
+    all_task_view,
+    completed_task_view,
+    done_task_view,
+    sessions_storage_view,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("tasks/", GenericTaskView.as_view()),
+    path("create-task/", GenericTaskCreateView.as_view()),
+    path("update-task/<pk>/", GenericTaskUpdateView.as_view()),
+    path("detail-task/<pk>/", GenericTaskDetailView.as_view()),
+    path("delete-task/<pk>/", GenericTaskDeleteView.as_view()),
+    path("user/signup", UserCreateView.as_view()),
+    path("user/login", UserLoginView.as_view()),
+    path("user/logout", LogoutView.as_view()),
+    path("completed_task/", completed_task_view),
+    path("completed_task/<int:index>/", done_task_view),
+    path("all_tasks/", all_task_view),
+    path("sessiontest/", sessions_storage_view),
 ]
